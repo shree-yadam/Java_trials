939a5f4a2d65b46b522369d682b6dc4562079faa
Shree Devi Yadam <shree.yadam@gmail.com>
2021-09-21 21:05:19 -0400

    variables

--- tutorial1/src/tutorial1/Application.java
+++ tutorial1/src/tutorial1/Application.java
@@ -3,7 +3,11 @@ package tutorial1;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		int myNumber;
+		
+		myNumber = 88;
+		
+		System.out.println(myNumber);
 	}
 
 }
