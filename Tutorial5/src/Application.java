44bca0acdc91901818501b0287bb5527315c40f5
Shree Devi Yadam <shree.yadam@gmail.com>
2021-09-22 11:45:53 -0400

    for loop

---
+++ Tutorial5/src/Application.java
@@ -0,0 +1,13 @@
+import java.util.Iterator;
+
+public class Application {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			
+			System.out.println("Hello");
+		}
+
+	}
+
+}
