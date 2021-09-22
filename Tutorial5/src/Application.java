4ba0590210e81969fc44ddf8af8bc6c9b8d83d03
Shree Devi Yadam <shree.yadam@gmail.com>
2021-09-22 11:48:32 -0400

    for loop with format specifier

--- Tutorial5/src/Application.java
+++ Tutorial5/src/Application.java
@@ -5,7 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
 			
-			System.out.println("Hello");
+			System.out.printf("The value of i is : %d\n", i);
 		}
 
 	}
