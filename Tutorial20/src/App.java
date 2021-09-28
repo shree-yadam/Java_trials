8411fbae40dd234a5ac3b4aec9f4e8a6410c5323
Shree Devi Yadam <shree.yadam@gmail.com>
2021-09-28 18:11:21 -0400

    String add

---
+++ Tutorial20/src/App.java
@@ -0,0 +1,14 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		String info = "";
+		
+		info += "My name is Bob.";
+		info += " ";
+		info += "I am a builder.";
+		
+		System.out.println(info);
+	}
+
+}
